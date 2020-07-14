c442c3f6454baf5bddef897b9346fc4957682178
HeibyBarahona <57502162+HeibyBarahona@users.noreply.github.com>
2020-07-13 18:41:12 -0600

    Primer commit-

---
+++ src/helpers/Juan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helpers;
+
+public class Juan extends DatosPersonales {
+
+    @Override
+    public String datosPersonales() {
+      return "Mi nombre es Juan con ID 0801199898765";
+    }
+      
+}
---
+++ src/helpers/Maria.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+public class Maria extends DatosPersonales {
+
+    @Override
+    public String datosPersonales() {
+    return "Mi nombre es Maria con ID 0801199567456";
+    }
+    
+}
